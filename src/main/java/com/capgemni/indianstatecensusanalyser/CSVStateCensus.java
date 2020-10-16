62397180b732dc098000b0ed9dad488ff82c96d2
bab21 <2016061@iiitdmj.ac.in>
2020-10-16 20:40:33 +0530

    loading csv data and returning number of entries in the file

---
+++ src/main/java/com/capgemni/indianstatecensusanalyser/CSVStateCensus.java
@@ -0,0 +1,23 @@
+package com.capgemni.indianstatecensusanalyser;
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStateCensus {
+	@CsvBindByName(column = "State")
+	public String state;
+	
+	@CsvBindByName(column = "Population")
+	public int population;
+	
+	@CsvBindByName(column ="AreaInSqKm")
+	public int areaInSqKm;
+	
+	@CsvBindByName(column ="DensityPerSqKm")
+	public int densityPerSqKm;
+	
+	@Override
+	public String toString() {
+		return "IndiaCensusCSV{" + "State='" + state + ", Population='" + population + ", AreaInSqKm='" + areaInSqKm
+				+ ", DensityPerSqKm='" + densityPerSqKm + '}';
+	}
+	
+}
---
+++ src/main/java/com/capgemni/indianstatecensusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,37 @@
+package com.capgemni.indianstatecensusanalyser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class StateCensusAnalyser {
+	
+	public int loadCSVData(String csvFilePath) {
+		int numOfEntries=0;
+		try {
+			Reader reader=Files.newBufferedReader(Paths.get(csvFilePath));
+			CsvToBean<CSVStateCensus> csvToBean=new CsvToBeanBuilder(reader).withType(CSVStateCensus.class)
+					.withIgnoreLeadingWhiteSpace(true)
+					.build();
+			Iterator<CSVStateCensus> censusCSVIterator = csvToBean.iterator();
+			Iterable<CSVStateCensus> csvIterable = () -> censusCSVIterator;
+			numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		return numOfEntries;
+	}
+	public static void main(String[] args) {
+		String csvFilePath="./StateCensusData.csv";
+		StateCensusAnalyser stateCensusAnalyser =new StateCensusAnalyser();
+		int numOfEntries = stateCensusAnalyser.loadCSVData(csvFilePath);
+		System.out.println("Number of Entries in file = "+numOfEntries);
+	}
+}
